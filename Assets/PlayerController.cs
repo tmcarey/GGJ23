4c913843a7c786e2ff678063edb01fe24e493468
Thomas Carey <69694382+tmcarey@users.noreply.github.com>
2023-02-03 20:50:46 -0500

    player movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,12 +1,23 @@
+using System;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    [Header("Setup")] 
-    [SerializeField] private Rigidbody2D rigidBody;
-    
-    [Header("Config")]
-    [SerializeField] private float moveSpeed = 10f;
+    [Header("Setup")] [SerializeField] private Rigidbody2D rigidBody;
+
+    [Header("Config")] [SerializeField] private float moveSpeed = 10f;
     [SerializeField] private float jumpForce = 10f;
+
+    private void OnMove(InputValue value)
+    {
+        var move = value.Get<Vector2>();
+        rigidBody.velocity = new Vector2(move.x * moveSpeed, rigidBody.velocity.y);
+    }
+
+    private void OnJump(InputValue value)
+    {
+        rigidBody.AddForce(jumpForce * Vector2.up, ForceMode2D.Impulse);
+    }
 
 }
